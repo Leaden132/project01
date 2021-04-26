8315ad638bee6541eceba18b0267bf96aa0c5445
Seunggi Lee <seunggi.lee21@gmail.com>
2021-04-26 15:10:29 -0400

    added comment section append

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-console.log("ok");
 
-const form = document.querySelector('form');
+const formElement = document.querySelector('.comment');
+console.log(formElement);
+const test = document.querySelector('#name');
+console.log(test);
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
+
+formElement.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputName = document.querySelector('#name');
+    const inputEmail = document.querySelector('#eMail');
+    const inputComment = document.querySelector('#comment');
+
+    const commentHTML = ` <div class="blogContainer">
+                        <img src="assets/comment-1.jpg" alt="commenter's profile">
+                        <div class="blogContainer2">
+                        <p>Tuesday October 9th, 2018 by ${inputName.value}</p>
+                        <p>${inputComment.value}</p>
+                        </div>
+                    </div>`;
+
+    const commentElement = document.querySelector('.additionalPost');
+    const comment = document.createElement('div');
+    comment.innerHTML = commentHTML;
+
+    commentElement.appendChild(comment);
+
+    inputName.value = '';
+    inputEmail.value ='';
+    inputComment.value ='';
 });
-
-const inputElement = document.querySelector('input');
-const task = inputElement.value;
